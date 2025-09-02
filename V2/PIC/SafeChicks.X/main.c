d8cc4c1e724726aa50d67a799884944c19b96155
wouter <thys_wouter@hotmail.com>
2025-09-02 16:17:14 +0200

    Now with interrupts on the button up pin

--- V2/PIC/SafeChicks.X/main.c
+++ V2/PIC/SafeChicks.X/main.c
@@ -45,25 +45,29 @@ volatile bool runFSM = false;
  *                      Function implementation 
  ******************************************************************************/
 
-void initialize(void) 
-{
+void initialize(void) {
     /* Oscillators setup */
-    OSCCONbits.IRCF = 0b100;        /* 1MHz */
-    OSCCONbits.SCS = 0b10;          /* Internal oscillator, RC_RUN power mode */
-    while(OSCCONbits.IOFS == 0);    /* Wait for OSC to be stable */
-    
+    OSCCONbits.IRCF = 0b100; /* 1MHz */
+    OSCCONbits.SCS = 0b10; /* Internal oscillator, RC_RUN power mode */
+    while (OSCCONbits.IOFS == 0); /* Wait for OSC to be stable */
+
     /* Port setup */
     TRISA = 0x00;
     TRISB = 0x00;
     TRISC = 0x00;
-    
+
     PORTA = 0x00;
     PORTB = 0x00;
     PORTC = 0x00;
-    
+
     /* Interrupt setup */
-    RCONbits.IPEN = 1;              /* Enable priority levels on interrupts   */
-    INTCONbits.PEIE = 1;            /* Enable all peripheral interrupts       */
+    INTCON2bits.INTEDG2 = 1; /* Interrupt on rising edge               */
+    INTCON3bits.INT2IP = 0; /* Low priority                           */
+    INTCON3bits.INT2IF = 0; /* Clear flag                             */
+    INTCON3bits.INT2IE = 1; /* Enables the INT2 external interrupt    */
+
+    RCONbits.IPEN = 1; /* Enable priority levels on interrupts   */
+    INTCONbits.PEIE = 1; /* Enable all peripheral interrupts       */
 
     /* My own code setups */
     D_TMR0_Init(TIMER_MODE_WORK);
@@ -71,45 +75,44 @@ void initialize(void)
     D_UART_Init();
     D_ADC_Init();
     C_FSM_Init(goToSleep);
-    
+
     /* Enable stuff */
     D_TMR0_Enable(true);
     D_UART_Enable(true);
-    INTCONbits.GIEH = 1;            /* Enables all high-priority interrupts   */
-    INTCONbits.GIEL = 1;            /* Enable low interrupts                  */
+    INTCONbits.GIEH = 1; /* Enables all high-priority interrupts   */
+    INTCONbits.GIEL = 1; /* Enable low interrupts                  */
 }
 
 void goToSleep(void) {
-    
+
     D_UART_Write("S\r\n");
-    
+
     /**
      * We will need peripheral clock so go to RC_IDLE mode on SLEEP
      */
-    
-    OSCCONbits.IDLEN = 1;           /* Idle mode on SLEEP instruction         */
-    
+
+    OSCCONbits.IDLEN = 1; /* Idle mode on SLEEP instruction         */
+
     D_TMR0_Init(TIMER_MODE_SLEEP);
     D_TMR0_Enable(true);
-    
+
     /* Lets go! */
     SLEEP();
 
     /* Wake up again */
     D_TMR0_Init(TIMER_MODE_WORK);
     D_TMR0_Enable(true);
-    
+
     D_UART_Write("W\r\n");
 }
 
-void main(void)
-{    
+void main(void) {
+    
     __delay_ms(100);
     initialize();
     __delay_ms(100);
 
-    while(1)
-    {
+    while (1) {
         if (runFSM) {
             runFSM = false;
             C_FSM_Tick();
@@ -117,18 +120,20 @@ void main(void)
     }
 }
 
-void __interrupt(low_priority) _LowInterruptManager (void)
-{
+void __interrupt(low_priority) _LowInterruptManager(void) {
     /* Check if TMR0 interrupt is enabled and if the interrupt flag is set */
-    if(INTCONbits.TMR0IE == 1 && INTCONbits.TMR0IF == 1)
-    {
+    if (INTCONbits.TMR0IE == 1 && INTCONbits.TMR0IF == 1) {
         runFSM = true;
         INTCONbits.TMR0IF = 0; /* clear the TMR0 interrupt flag */
     }
+    /* Check if INT2 interrupt is enabled and if the interrupt flag is set */
+    if (INTCON3bits.INT2IE == 1 && INTCON3bits.INT2IF == 1) {
+        runFSM = true;
+        INTCON3bits.INT2IF = 0; /* clear the INT2 interrupt flag */
+    }
 }
 
-void __interrupt(high_priority) _HighInterruptManager (void)
-{
+void __interrupt(high_priority) _HighInterruptManager(void) {
 
 }
 
